a26552d3267fcc53777807e9fe397fc70460ef41
Frederic Jacobs <frederic.jacobs@mac.com>
2012-08-11 23:32:15 -0700

    Revert "new token"
    This reverts commit f5c3a64ee18d7c7acef2be320b899d204a678b3f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:token_authenticatable
-  before_save :ensure_authentication_token
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
