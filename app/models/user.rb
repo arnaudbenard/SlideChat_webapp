8c38dba888ef183a246b061076f8fdde83ffb651
Frederic Jacobs <frederic.jacobs@mac.com>
2012-08-12 00:38:09 -0700

    Auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
