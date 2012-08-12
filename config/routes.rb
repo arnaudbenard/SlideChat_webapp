3488a8e1a231c6c5aca5cffe95aac02f09abe18d
Frederic Jacobs <frederic.jacobs@mac.com>
2012-08-11 23:32:15 -0700

    Revert "new implementation of login"
    This reverts commit c5a61afac0d8860fc846b150a528b496fa60ab89.

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,11 @@ OpentokSession::Application.routes.draw do
       get :auth
     end
   end
-match '/login',       :to => 'pages#login'
- 
+
+namespace :api do
+  devise_for :users
+  resources :recipes, :only=>[:index, :show]
+end  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
