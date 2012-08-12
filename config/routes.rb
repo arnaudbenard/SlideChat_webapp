0dafc0057cb3f31431bc3199eba723d5530aede0
Arnaud Benard <arnaudbenard13@gmail.com>
2012-08-11 22:51:20 -0700

    removed forgery

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ OpentokSession::Application.routes.draw do
       get :auth
     end
   end
-match '/login',       :to => 'pages#login'
+  match '/login',       :to => 'pages#login'
  
   # The priority is based upon order of creation:
   # first created -> highest priority.
